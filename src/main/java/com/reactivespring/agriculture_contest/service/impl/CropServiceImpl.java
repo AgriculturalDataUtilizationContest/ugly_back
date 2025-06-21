7bd0b44d393521c97de69246b925545919a4b8a4
7SH7 <kimseunghwan7777@gmail.com>
2025-06-21 18:37:01 +0900

    Refactor: 손가는 대로 작성한 코드 제거

--- src/main/java/com/reactivespring/agriculture_contest/service/impl/CropServiceImpl.java
+++ src/main/java/com/reactivespring/agriculture_contest/service/impl/CropServiceImpl.java
@@ -16,20 +16,6 @@ public class CropServiceImpl implements CropService {
 
     @Override
     public CropDto.ForecastResDto getForecastCropDetails(CropDto.ForecastReq forecastReq) {
-//        ArrayList<CropDto.ForecastCropInfo> forecastResDtos = new ArrayList<>();
-//        String category = forecastReq.getCategory();
-//        // 이 부분 배웠다..! 오..?
-//        cropRepository.findByCategory(category).forEach(crop -> {
-//            forecastResDtos.add(CropDto.ForecastCropInfo.builder()
-//                    .cropsName(crop.getCropKorName())
-//                    .cropsImage(crop.getCropsImage())
-//                    .build());
-//        });
-//
-//        CropDto.ForecastResDto temp = CropDto.ForecastResDto.builder().crops(forecastResDtos).build();
-//        temp.setCrops(forecastResDtos);
-//        return temp;
-
         CropDto.ForecastResDto forecastResDtos = CropDto.ForecastResDto.builder().crops(new ArrayList<>()).build();
 
         cropRepository.findByCategory(forecastReq.getCategory()).forEach(crop -> {
