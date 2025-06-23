da72cb288402490fb6f20299340d3779e0ab7200
7SH7 <kimseunghwan7777@gmail.com>
2025-06-23 21:21:56 +0900

    feat: set scheduled (00:00, 00:10 everyday)

--- src/main/java/com/reactivespring/agriculture_contest/controller/RegularExecutionController.java
+++ src/main/java/com/reactivespring/agriculture_contest/controller/RegularExecutionController.java
@@ -2,6 +2,7 @@ package com.reactivespring.agriculture_contest.controller;
 
 import com.reactivespring.agriculture_contest.service.RegularExecutionService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,13 @@ public class RegularExecutionController {
         this.regularExecutionService = regularExecutionService;
     }
 
+    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
     @GetMapping("/update/kamis/data")
     public ResponseEntity<Void> updateKamisData() {
         return ResponseEntity.ok(regularExecutionService.updateKamisData());
     }
 
+    @Scheduled(cron = "0 10 0 * * *", zone = "Asia/Seoul")
     @GetMapping("/update/pred/data")
     public ResponseEntity<Void> updatePredData() {
         return ResponseEntity.ok(regularExecutionService.updatePredData());
